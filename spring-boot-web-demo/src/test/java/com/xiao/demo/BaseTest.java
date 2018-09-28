355d82d599b278db87726e48979bd114e68594cc
tianxiaoliang <tianxiaoliang@meituan.com>
2018-09-28 14:06:48 +0800

    boot web

--- spring-boot-web-demo/src/test/java/com/xiao/demo/BaseTest.java
+++ spring-boot-web-demo/src/test/java/com/xiao/demo/BaseTest.java
@@ -1,13 +1,7 @@
 package com.xiao.demo;
 
-import com.xiao.demo.boot.BootStarter;
-import com.xiao.demo.boot.WebStarter;
-import junit.framework.Test;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
 import org.junit.runner.RunWith;
-import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
