49fa72f83c6c632613499a07d52252453a17a698
tianxiaoliang <tianxiaoliang@meituan.com>
2018-09-30 11:22:03 +0800

    boot web

--- spring-boot-web-demo/src/main/java/com/xiao/demo/boot/controller/UserController.java
+++ spring-boot-web-demo/src/main/java/com/xiao/demo/boot/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/user")
+@RequestMapping(value = "/api/v1/user")
 public class UserController {
 
     @Autowired
